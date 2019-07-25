2548498ccac0888d72116d233f5591119ca87fb5
Nicolas Daudin <nicolas.daudin@gmail.com>
2019-07-25 12:12:06 +0200

    Adding production environment in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,8 @@ if (process.env.NODE_ENV === 'test'){
   require('dotenv').config({path: '.env.test'});
 } else if (process.env.NODE_ENV === 'development'){
   require('dotenv').config({path: '.env.development'});
+} else { // production
+  require('dotenv').config({path: '.env.development'});
 }
 
 module.exports = (env) => {
